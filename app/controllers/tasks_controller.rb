edf394a9aa1aeb412a6341fd56fb7795d51ff395
n.mitani <gonesh4747@gmail.com>
2021-01-14 21:06:28 +0000

    f-index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,9 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy,:edit,:update]
+    before_action :correct_user, only: [:show,:destroy,:edit,:update]
     
     def index
-        if logged_in?
-        @task=current_user.tasks.build
         @tasks=current_user.tasks.order(id: :desc)
-        end
         
     end
     
